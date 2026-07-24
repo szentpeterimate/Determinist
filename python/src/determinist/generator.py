51f9c26c18763f0cec59b4d9d486a4696abf0f91
Szentpéteri Máté <szentpeteri.mate1166@gmail.com>
2026-07-24 14:32:16 +0200

    Refactor for future algorithms

--- python/src/determinist/generator.py
+++ python/src/determinist/generator.py
@@ -1,40 +1,18 @@
-import argon2
-from argon2.low_level import Type, hash_secret
-from typing import Literal, final
+from typing import Literal
 from string import punctuation
+from .algorithms import v1
 
 class Generator:
-    def __init__(self, char_map: dict[str, str] = {'a': '$', 'h': '(', 'z': '&'}, spec_chars: list = list(punctuation), spec_mode: Literal["replace", "insert"] = "insert", pass_length: int = 8, spec_freq: int = 4) -> None:
+    def __init__(self, version: int, char_map: dict[str, str] = {'a': '$', 'h': '(', 'z': '&'}, spec_chars: list = list(punctuation), spec_mode: Literal["replace", "insert"] = "insert", pass_length: int = 8, spec_freq: int = 4) -> None:
+        self.version = version
         self.char_map = char_map
         self.spec_chars = spec_chars
         self.pass_length = pass_length
-        self.spec_mode = spec_mode
+        self.spec_mode: Literal["replace", "insert"] = spec_mode
         self.spec_freq = spec_freq
 
     def generate_password(self, master_pass: str, site_name: str) -> str:
-        hashed_str = hash_secret(secret=master_pass.encode('utf-8'),
-                                 salt=site_name.encode('utf-8'),
-                                 time_cost=2,
-                                 memory_cost=65536,
-                                 parallelism=4,
-                                 hash_len=32,
-                                 type=Type.ID
-                                ).decode('utf-8')
-        stripped = hashed_str.split('$')[-1]
-
-        chars_added = ""
-        if self.spec_mode == "insert":
-            for i, char in enumerate(stripped):
-                chars_added += char
-                if (i + 1) % self.spec_freq == 0:
-                    chars_added += self.spec_chars[i % len(self.spec_chars)]
-        elif self.spec_mode == "replace":
-            for _, char in enumerate(stripped):
-                if char.lower() in self.char_map:
-                    chars_added += self.char_map[char.lower()]
-                else:
-                    chars_added += char
-
-        final_password = chars_added[-self.pass_length:]
-
-        return final_password
+        if self.version == 1:
+            return v1.generate(master_pass=master_pass, site_name=site_name, pass_length=self.pass_length, spec_mode=self.spec_mode, spec_chars=self.spec_chars, char_map=self.char_map, spec_freq=self.spec_freq)
+        else:
+            raise ValueError(f"Unsupported algorithm version: {self.version}")
--- python/tests/test_v1.py
+++ python/tests/test_v1.py
@@ -5,27 +5,27 @@ from unittest import TestCase
 
 class TestGenV1(TestCase):
     def test_deterministic_output(self):
-        gen = Generator()
+        gen = Generator(version=1)
         self.assertEqual(gen.generate_password("Very-Strong-Passphrase", "github.com"), "gZR5(UQQ")
 
     def test_site_changes_output(self):
-        gen = Generator()
+        gen = Generator(version=1)
         self.assertEqual(gen.generate_password("Very-Strong-Passphrase", "reddit.com"), "LkW7(WnU")
 
     def test_master_changes_output(self):
-        gen = Generator()
+        gen = Generator(version=1)
         self.assertEqual(gen.generate_password("Another-Very-Strong-Passphrase", "github.com"), "20Ps(XiE")
 
     def test_length(self):
-        gen = Generator(pass_length=12)
+        gen = Generator(version=1, pass_length=12)
         self.assertEqual(len(gen.generate_password("Very-Strong-Passphrase", "github.com")), 12)
 
     def test_replace(self):
-        gen = Generator(spec_mode="replace", char_map={'g': '{', 'q': '.', 'r': '='})
+        gen = Generator(version=1, spec_mode="replace", char_map={'g': '{', 'q': '.', 'r': '='})
         self.assertEqual(gen.generate_password("Very-Strong-Passphrase", "github.com"), "V{Z=5U..")
 
     def test_frequency(self):
-            gen = Generator(spec_freq=2, pass_length=16)
+            gen = Generator(version=1, spec_freq=2, pass_length=16)
             self.assertEqual(gen.generate_password("Very-Strong-Passphrase", "github.com"), "wZ\"LV$gZ&R5(UQ*Q")
         
 if __name__ == "__main__":
