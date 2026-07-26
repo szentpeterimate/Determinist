e73ad7fd6794f3b82d8261ce8f92da06d61e070e
Szentpéteri Máté <szentpeteri.mate1166@gmail.com>
2026-07-26 03:04:50 +0200

    Add v2 compatibility

--- python/src/determinist/generator.py
+++ python/src/determinist/generator.py
@@ -1,9 +1,9 @@
-from typing import Literal
+from typing import Literal, List
 from string import punctuation
-from .algorithms import v1
+from .algorithms import v1, v2
 
 class Generator:
-    def __init__(self, version: int, char_map: dict[str, str] = {'a': '$', 'h': '(', 'z': '&'}, spec_chars: list = list(punctuation), spec_mode: Literal["replace", "insert"] = "insert", pass_length: int = 8, spec_freq: int = 4) -> None:
+    def __init__(self, version: int, char_map: dict[str, str] = {'a': '$', 'h': '(', 'z': '&'}, spec_chars: list = list(punctuation), spec_mode: Literal["replace", "insert"] = "insert", pass_length: int = 8, spec_freq: int = 4, char_types: list[Literal["special", "lowercase", "uppercase", "digits"]] = ["special", "lowercase", "uppercase", "digits"]) -> None:
         self.version = version
         self.char_map = char_map
         self.spec_chars = spec_chars
@@ -14,5 +14,7 @@ class Generator:
     def generate_password(self, master_pass: str, site_name: str) -> str:
         if self.version == 1:
             return v1.generate(master_pass=master_pass, site_name=site_name, pass_length=self.pass_length, spec_mode=self.spec_mode, spec_chars=self.spec_chars, char_map=self.char_map, spec_freq=self.spec_freq)
+        if self.version == 1:
+            return v2.generate(master_pass=master_pass, site_name=site_name, pass_length=self.pass_length, spec_mode=self.spec_mode, spec_chars=self.spec_chars, char_map=self.char_map, spec_freq=self.spec_freq, char_types=[])
         else:
             raise ValueError(f"Unsupported algorithm version: {self.version}")
