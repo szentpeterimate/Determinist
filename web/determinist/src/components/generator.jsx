5b9d7aa9eaffd37203ca96ee1f3de3cc3b9bf1b7
Szentpéteri Máté <szentpeteri.mate1166@gmail.com>
2026-08-12 00:06:52 +0200

    Add v2 generator algorithm

--- web/determinist/src/components/generator.jsx
+++ web/determinist/src/components/generator.jsx
@@ -2,19 +2,28 @@
 
 import argon2 from 'argon2-browser/dist/argon2-bundled.min.js'
 import CharMapInput from './charmapinput';
+import CharSetInput from './charsetinput';
 import { useState } from 'react'
+import { PythonRandom } from './python-random'
+
+const encoder = new TextEncoder()
 
 export default function Generator() {
-    const [masterpass, setMasterpass] = useState("");
-    const [sitename, setSitename] = useState("");
-    const [passlength, setPassLength] = useState();
-    const [specmode, setSpecMode] = useState("");
-    const [charmap, setCharMap] = useState({});
-    const [specchars, setSpecChars] = useState("");
-    const [specfreq, setSpecFreq] = useState();
+    const [masterpass, setMasterpass] = useState("")
+    const [sitename, setSitename] = useState("")
+    const [passlength, setPassLength] = useState(0)
+    const [version, setVersion] = useState(0)
+    const [specmode, setSpecMode] = useState("")
+    const [charmap, setCharMap] = useState({})
+    const [specchars, setSpecChars] = useState("")
+    const [specfreq, setSpecFreq] = useState(0)
+    const [charset, setCharSet] = useState(["lowercase", "uppercase", "digits", "special"])
     const [hash, setHash] = useState("")
 
     const punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    const asciiLowercase = "abcdefghijklmnopqrstuvwxyz"
+    const asciiUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const digits = "0123456789"
 
     async function v1(masterPass, siteName, passLength = 8, specMode = "insert", charMap = {}, specChars = punctuation, specFreq = 4) {
         const hashedString = await argon2.hash({
@@ -51,11 +60,70 @@ export default function Generator() {
         return finalPassword
     }
 
+    async function v2(masterPass, siteName, passLength = 8, charTypes = ["digits", "special", "lowercase", "uppercase"]) {
+        const typesSet = new Set(charTypes)
+        let charSet = ""
+
+        if (typesSet.has("special")) {
+            charSet += punctuation
+        }
+        if (typesSet.has("lowercase")) {
+            charSet += asciiLowercase
+        }
+        if (typesSet.has("uppercase")) {
+            charSet += asciiUppercase
+        }
+        if (typesSet.has("digits")) {
+            charSet += digits
+        }
+
+        const salt = masterPass + siteName + String(passLength) + "{}" + charSet
+        
+        const rng = new PythonRandom(salt)
+        
+        const charSetList = rng.shuffle(charSet.split(''))
+        
+        const hashedBytes = await argon2.hash({
+            pass: encoder.encode(masterPass),
+            salt: encoder.encode(salt),
+            time: 2,
+            mem: 65536,
+            parallelism: 4,
+            hashLen: 32*passLength,
+            type: argon2.ArgonType.Argon2id
+        })
+
+        const stream = encoder.encode(hashedBytes.encoded) 
+
+        let password = ""
+        const limit = 256 - (256 % charSetList.length)
+        for (let i = 0; i < stream.length; i++) {
+            const byte = stream[i]
+            if (password.length == passLength) {
+                break
+            } else if (byte < limit) {
+                password += charSetList[byte % charSetList.length]
+            }
+        }
+        let passwordToShuffle = password.split('')
+        const shuffled_pass = rng.shuffle(passwordToShuffle)
+        console.log(JSON.stringify(Array.from(shuffled_pass)))
+        const final = shuffled_pass.join('')
+        
+        return final
+    }
+
     async function handleGenerate(e) {
         e.preventDefault()
 
-        const result = await v1(masterpass, sitename, passlength, specmode, charmap, specchars, specfreq)
-        console.log(charmap)
+        let result = ""
+
+        if (version == 1) {
+            result = await v1(masterpass, sitename, passlength, specmode, charmap, specchars, specfreq)
+        } else if (version == 2) {
+            result = await v2(masterpass, sitename, passlength, charset)
+        }
+
         setHash(result)
     }
 
@@ -80,6 +148,14 @@ export default function Generator() {
             />
             <input 
                 type="number" 
+                name='version' 
+                value={version}
+                onChange={(e) => setVersion(e.target.value)}
+                placeholder="Algorithm Version"
+                autoComplete='off'
+            />
+            <input 
+                type="number" 
                 name='passlength' 
                 value={passlength}
                 onChange={(e) => setPassLength(e.target.value)}
@@ -95,7 +171,10 @@ export default function Generator() {
                 autoComplete='off'
             />
             <div id="charmapinput">
-                <CharMapInput onChange={(dictResult) => setCharMap(dictResult)}></CharMapInput>
+                <CharMapInput onChange={(dictResult) => setCharMap(dictResult)} />
+            </div>
+            <div id="charmapinput">
+                <CharSetInput onChange={(sets) => setCharSet(sets)} />
             </div>
             <input 
                 type="text" 
@@ -115,7 +194,7 @@ export default function Generator() {
             />
             <button type='submit'>Generate</button>
         </form>
-        <p id='result'>{hash}</p> 
+        <p id='result'>{hash}</p>
     </div>
   );
 }
