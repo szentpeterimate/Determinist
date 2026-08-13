bfabc973006186eeaab3a37bbf28c4dc3271ee87
Szentpéteri Máté <szentpeteri.mate1166@gmail.com>
2026-08-13 12:09:49 +0200

    Set assetPath and assetPrefix for production

--- web/determinist/next.config.mjs
+++ web/determinist/next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   reactCompiler: true,
+  asePath: process.env.NODE_ENV === 'production' ? '/determinist' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/determinist' : '',
 };
 
 export default nextConfig;
